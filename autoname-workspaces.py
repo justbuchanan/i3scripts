63408428d063172c4f66b3f683c8514be9acf558
Justin Buchanan <justbuchanan@gmail.com>
2016-04-26 02:53:00 -0400

    comments

--- autoname-workspaces.py
+++ autoname-workspaces.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+# This script listens for i3 events and updates workspace names to show icons
+# for running programs.
+
 import i3ipc
 import subprocess as proc
 import re
@@ -19,6 +22,7 @@ def xprop(win_id, property):
 
 
 def icon_for_window(window):
+    # add icons to this list for common programs you use
     icons = {
         'urxvt': '\uf120',
         'google-chrome': '\uf268',
@@ -27,6 +31,7 @@ def icon_for_window(window):
     cls = xprop(window.window, 'WM_CLASS')
     return icons[cls] if cls in icons else '*'
 
+# renames all workspaces based on the windows present
 def rename():
     for workspace in i3.get_tree().workspaces():
         icons = [icon_for_window(w) for w in workspace.leaves()]
@@ -34,10 +39,11 @@ def rename():
         new_name = str(workspace.num) + ': ' + '  '.join(icons)
         i3.command('rename workspace "%s" to "%s"' % (workspace.name, new_name))
 
+rename()
+
+# call rename() for relevant window events
 def on_change(i3, e):
     if e.change in ['new', 'close', 'move']:
         rename()
-
-rename()
 i3.on('window', on_change)
 i3.main()
