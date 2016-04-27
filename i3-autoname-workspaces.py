ab2888a45a5630021c08f6744786d2331695111d
Justin Buchanan <justbuchanan@gmail.com>
2016-04-26 21:09:06 -0400

    minor refactor

--- i3-autoname-workspaces.py
+++ i3-autoname-workspaces.py
@@ -7,10 +7,30 @@ import i3ipc
 import subprocess as proc
 import re
 
+
+# Add icons here for common programs you use.  The keys are the X window class
+# names and the icons can be any text you want to display. However most of
+# these are character codes for font awesome:
+#   http://fortawesome.github.io/Font-Awesome/icons/
+WINDOW_ICONS = {
+    'urxvt': '\uf120',
+    'google-chrome': '\uf268',
+    'subl': '\uf1c9',
+    'subl3': '\uf1c9',
+    'spotify': '\uf001',
+    'Firefox': '\uf269',
+    'libreoffice': '\uf0f6',
+    'feh': '\uf03e',
+    'mupdf': '\uf1c1',
+    'evince': '\uf1c1',
+}
+
+
 i3 = i3ipc.Connection()
 
+# Returns an array of the values for the given property from xprop.  This
+# requires xorg-xprop to be installed.
 def xprop(win_id, property):
-    # note: requires xorg-xprop to be installed
     try:
         prop = proc.check_output(['xprop', '-id', str(win_id), property])
         prop = prop.decode('utf-8')
@@ -21,25 +41,10 @@ def xprop(win_id, property):
 
 
 def icon_for_window(window):
-    # Add icons to this list for common programs you use.
-    # Most of these are character codes for font awesome:
-    #   http://fortawesome.github.io/Font-Awesome/icons/
-    icons = {
-        'urxvt': '\uf120',
-        'google-chrome': '\uf268',
-        'subl': '\uf1c9',
-        'subl3': '\uf1c9',
-        'spotify': '\uf001',
-        'Firefox': '\uf269',
-        'libreoffice': '\uf0f6',
-        'feh': '\uf03e',
-        'mupdf': '\uf1c1',
-        'evince': '\uf1c1',
-    }
     classes = xprop(window.window, 'WM_CLASS')
     for cls in classes:
-        if cls in icons:
-            return icons[cls]
+        if cls in WINDOW_ICONS:
+            return WINDOW_ICONS[cls]
     print('No icon available for window with classes: %s' % str(classes))
     return '*'
 
@@ -47,8 +52,7 @@ def icon_for_window(window):
 def rename():
     for workspace in i3.get_tree().workspaces():
         icons = [icon_for_window(w) for w in workspace.leaves()]
-
-        new_name = str(workspace.num) + ': ' + '  '.join(icons)
+        new_name = "%d: %s" % (workspace.num, '  '.join(icons))
         i3.command('rename workspace "%s" to "%s"' % (workspace.name, new_name))
 
 rename()
